0a892e0ea7c11c43d1b330d5a5b64501fd9b5748
abel-tefera <abeltefera16@gmail.com>
2023-06-22 11:35:09 +0300

    Created book class with relationship to rentals

---
+++ book.rb
@@ -0,0 +1,15 @@
+class Book
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def rentals(person, date)
+    Rental.new(date, self, person)
+  end
+
+  attr_accessor :title, :author
+
+end
