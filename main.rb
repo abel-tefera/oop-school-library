9891d36457a6501cd5df7506643f47f794abf5c1
abel-tefera <abeltefera16@gmail.com>
2023-06-22 17:09:12 +0300

    Set up main.rb as entry point

--- main.rb
+++ main.rb
@@ -1,11 +1,44 @@
-require_relative 'person'
-require_relative 'base_decorator'
-require_relative 'trimmer_decorator'
-require_relative 'capitalize_decorator'
+require_relative 'app'
 
-person = Person.new(22, 'maximilianus')
-puts "Undecorated Name is '#{person.correct_name}'"
-caps_name = CapitalizeDecorator.new(person)
-puts "Capitalized Name is '#{caps_name.correct_name}'"
-trimmed_name = TrimmerDecorator.new(caps_name)
-puts "Trimmed, Capitalized Name is '#{trimmed_name.correct_name}'"
+def menu(app)
+  app.options
+  print 'Prompt > '
+  gets.chomp.to_i
+end
+
+def manage_selection(app, option)
+  tasks = {
+    1 => :list_books,
+    2 => :list_persons,
+    3 => :create_person,
+    4 => :create_book,
+    5 => :create_rental,
+    6 => :list_rental,
+    7 => :exit,
+    default: :invalid_option
+  }
+
+  puts 'Thank you for using this app!' if option == 7
+
+  selection = tasks[option] || tasks[:default]
+  app.send(selection)
+
+  return unless option == 7
+
+  exit
+end
+
+def main
+  app = App.new
+
+  puts '------------------------------'
+  puts 'Welcome to School Library App!'
+  puts '------------------------------'
+
+  loop do
+    manage_selection(app, menu(app))
+    puts "\n"
+  end
+end
+
+main
