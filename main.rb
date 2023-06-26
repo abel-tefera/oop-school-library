8773f58561bc910dd902534f7b737c4ad18882f7
abel-tefera <abeltefera16@gmail.com>
2023-06-26 17:09:50 +0300

    Main entry point refactored

--- main.rb
+++ main.rb
@@ -1,44 +1,10 @@
 require_relative 'app'
-
-def menu(app)
-  app.options
-  print 'Prompt > '
-  gets.chomp.to_i
-end
-
-def manage_selection(app, option)
-  tasks = {
-    1 => :list_books,
-    2 => :list_persons,
-    3 => :create_person,
-    4 => :create_book,
-    5 => :create_rental,
-    6 => :list_rental,
-    7 => :exit,
-    default: :invalid_option
-  }
-
-  puts 'Thank you for using this app!' if option == 7
-
-  selection = tasks[option] || tasks[:default]
-  app.send(selection)
-
-  return unless option == 7
-
-  exit
-end
+require_relative 'home'
 
 def main
   app = App.new
 
-  puts '------------------------------'
-  puts 'Welcome to School Library App!'
-  puts '------------------------------'
-
-  loop do
-    manage_selection(app, menu(app))
-    puts "\n"
-  end
+  home(app)
 end
 
 main
